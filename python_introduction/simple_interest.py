f089dc3df83ed0fac8a3ecded57f12231da45902
nyk-o-brown <odoyobrown3@gmail.com>
2025-07-12 23:20:50 +0300

    msg:test2

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,9 +3,9 @@ rate =  0.05
 time = 3 #years
 
 
-#intrest = principle * rate * time
+intrest = principal * rate * time
 
-print (f"The simple intrest is: [{ principal * rate * time}]")
+print (f"The simple intrest is: [{ intrest }]")
 
 
 
