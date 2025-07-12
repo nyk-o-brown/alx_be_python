4e00f9e2bfcc6e022ddfaa05a8364fed1e57ebb9
nyk-o-brown <odoyobrown3@gmail.com>
2025-07-12 17:34:15 +0300

    Mand S

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -18,10 +18,10 @@ Operation = input("Enter the operation :")
 
 #display the data accordint to the users option
 if Operation == "Addition":
-    print(f"The sum of {number1} and {number2} is {Addition}")
+    print(f"Sum of {number1} and {number2} is {Addition}")
 
 if Operation == "Subtraction":
-    print(f"The subtraction of {number1} and {number2} is {Subtraction}")
+    print(f"Subtraction of {number1} and {number2} is {Subtraction}")
     
 if Operation == "Multiplication":
-    print(f"The multiplication of {number1} and {number2} is {Multiplication}")        
+    print(f"Multiplication of {number1} and {number2} is {Multiplication}")        
