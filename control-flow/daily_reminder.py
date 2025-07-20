936ab48095c15d1d9da3d32e4d0e87ed71357835
nyk-o-brown <odoyobrown3@gmail.com>
2025-07-20 23:09:27 +0300

    msg:spelling

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,23 +3,23 @@ task = input("Enter your task:")
 
 priority = input("Priority (high/medium/low):")
 
-time = input("Is it time-bound? (yes/no):")
+time_bound = input("Is it time-bound? (yes/no):")
 
 match priority  :
     case "high" :
-        if time == "yes" :
+        if time_bound == "yes" :
             print(f"Reminder: '{task}' is a {priority} task that requires immediate attention today! ")
         else:
             print(f"Reminder: '{task}' is a {priority} task that is a low priority task. Consider completing it when you have free time. ")   
         
     case "medium" :
-        if time == "yes" :
+        if time_bound == "yes" :
             print(f"Reminder: '{task}' is a {priority} task that requires immediate attention today! ")
         else:
             print(f"Reminder: '{task}' is a {priority} task that is a low priority task. Consider completing it when you have free time. ") 
     
     case "low" :
-        if time == "yes" :
+        if time_bound == "yes" :
             print(f"Reminder: '{task}' is a {priority} task that requires immediate attention today! ")
         else:
             print(f"Reminder: '{task}' is a {priority} task that is a low priority task. Consider completing it when you have free time. ")                     
