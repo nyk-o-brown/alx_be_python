9fe6919f561e92be1e0c44e9007f5d154c3c5693
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-03 11:59:12 +0300

    msg:bad biscuits made the baker go broke

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,10 +1,11 @@
 class BankAccount:
-    def __init__(self , initial_balance= 0 ):
+    def __init__(self , initial_balance= 0 , ):
         
         self.__account_balance = initial_balance
         
+        
     def deposit(self, amount) :
-        self.__account_balance = self.__account_balance + amount
+        self.__account_balance += amount
             
     def withdraw(self ,amount) :
         if amount < self.__account_balance :
@@ -13,4 +14,4 @@ class BankAccount:
             return False 
           
     def display_balance (self) :
-        print(f"Your account balance is {self.__account_balance}")      
+        print(f"Your Current balance : {self.__account_balance} is low.")      
