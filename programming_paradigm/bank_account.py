4114e94b4d6622e23b6853fe776458205ca4e203
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-03 15:12:50 +0300

    msg:lyu

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,10 +8,11 @@ class BankAccount:
         self.__account_balance += amount
             
     def withdraw(self ,amount) :
-        if amount <= self.__account_balance :
+        if amount <= self.__account_balance:
             self.__account_balance -= amount
+            return True
         else:    
             return False 
           
     def display_balance (self) :
-        print(f"Your Current Balance: {self.__account_balance} is low.")      
+        print(f"Current Balance: ${self.__account_balance:.2f}")      
