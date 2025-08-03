81500159b21d499ca73065b66bf9e3ed4c028bb4
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-03 18:13:34 +0300

    msg: value error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,7 +5,7 @@ def safe_divide (numerator , denominator) :
          return f"The result of the division is {result}"
     except ZeroDivisionError:
         return"Error: Cannot divide by zero."  
-    except TypeError:
+    except (TypeError , ValueError):
         return"Error: Please enter numeric values only."   
    
    
