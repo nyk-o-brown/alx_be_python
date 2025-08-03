62e9743b9ba0bef1fe407635cea9a0537af16e47
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-03 22:00:24 +0300

    msg: svj

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
