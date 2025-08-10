890f4a8d25ea21461f93c68036b18cb34a3491a5
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-10 18:14:33 +0300

    msh:lao

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,10 +5,10 @@ class Book:
         self.year=year
         
     def __str__(self):
-        return f"'{self.title}' ,'{self.author}','{self.year}'"
+        return f"'{self.title}'by ,'{self.author}', published in '{self.year}'"
     
     def __repr__(self):
-         return f"'{self.title}' ,'{self.author}','{self.year}'"
+         print (f"'{self.title}' ,'{self.author}','{self.year}'")
     
     def __del__(self):
-        print(f"Deleting {self.title}")  
+        return f"Deleting {self.title}"  
