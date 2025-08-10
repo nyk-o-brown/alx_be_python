469c0d63d46e33bada85a78e38a7086d7e996c96
nyk-o-brown <odoyobrown3@gmail.com>
2025-08-10 19:44:04 +0300

    msg:kew

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,42 +1,38 @@
 class Book:
     def __init__(self, title, author):
-        
-       self.title = title
-       self.author = author
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
+    
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
-               
+    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library:
     def __init__(self):
-        self.books = []  # will hold Book, EBook, PrintBook instances
-
+        self.books = []
+    
     def add_book(self, book):
         """Add any Book-derived object to the library."""
         self.books.append(book)
-
+    
     def list_books(self):
         """Print details of each book in the library."""
         for book in self.books:
-            # Decide how to display based on the book’s class
-            if isinstance(book, EBook):
-                def __str__(self):
-                    print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            
-            elif isinstance(book, PrintBook):
-                def __str__(self):
-                    print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-                
-            else:
-                def __str__(self):
-                    print(f"Book: {book.title} by {book.author}")
-                
+            print(str(book))
 
-                      
+
